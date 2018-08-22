8941c7a0b1fb80482d3c2248225988f0ae6d6daa
= <sankalpjonna@gmail.com>
2018-08-22 23:11:47 +0530

    code formatted and vetted, test case made better

--- pen.go
+++ pen.go
@@ -1,82 +1,82 @@
 package pen
 
 import (
-  "log"
-  "os"
-  "github.com/lestrrat-go/strftime"
-  "time"
+	"github.com/lestrrat-go/strftime"
+	"log"
+	"os"
+	"time"
 )
 
 type Pen struct {
-  // thread save logger instance
-  logger *log.Logger
+	// thread save logger instance
+	logger *log.Logger
 
-  // file
-  fp *os.File
+	// file
+	fp *os.File
 
-  // log prefix
-  prefix string
+	// log prefix
+	prefix string
 
-  // location of file
-  location *strftime.Strftime
+	// location of file
+	location *strftime.Strftime
 
-  // time location
-  tloc   *time.Location
+	// time location
+	tloc *time.Location
 
-  // file name created by builder
-  path string
+	// file name created by builder
+	path string
 }
 
 func New(prefix string, location string) *Pen {
-  p, err := strftime.New(location)
+	p, err := strftime.New(location)
 	if err != nil {
 		panic(err)
 	}
 
 	return &Pen{
 		location: p,
-		tloc: time.Local,
-    prefix: prefix,
+		tloc:     time.Local,
+		prefix:   prefix,
 	}
 }
 
 func (self *Pen) initLogger() error {
-  ct := time.Now()
-  path := self.location.FormatString(ct.In(self.tloc))
+	ct := time.Now()
+	path := self.location.FormatString(ct.In(self.tloc))
 
-  if self.logger == nil {
-    fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-  	if err != nil {
-  		return err
-  	}
-    self.fp = fp
-    self.logger = log.New(self.fp, "", 0)
-    self.logger.SetPrefix(time.Now().Format("2006-01-02 15:04:05") + " [" + self.prefix + "] ")
-
-    self.path = path
-  } else if self.path != path {
-    if self.fp != nil {
-			self.fp.Close()
-      self.fp = nil
-      self.logger = nil
-      return self.initLogger()
+	if self.logger == nil {
+		fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			return err
 		}
-  }
-  return nil
+		self.fp = fp
+		self.logger = log.New(self.fp, "", 0)
+		self.logger.SetPrefix(time.Now().Format("2006-01-02 15:04:05") + " [" + self.prefix + "] ")
+
+		self.path = path
+	} else if self.path != path {
+		if self.fp != nil {
+			self.fp.Close()
+			self.fp = nil
+			self.logger = nil
+			return self.initLogger()
+		}
+	}
+	return nil
 }
 
 func (self *Pen) Write(content string) error {
-  err := self.initLogger()
-  if err != nil {
-    return err
-  }
+	err := self.initLogger()
+	if err != nil {
+		return err
+	}
 	self.logger.Println(content)
-  return nil
+	return nil
 }
 
 func (self *Pen) Lid() {
-  if self.fp != nil {
-    self.fp.Close()
-    self.logger = nil
-  }
+	if self.fp != nil {
+		self.fp.Close()
+		self.logger = nil
+	}
 }
--- pen_test.go
+++ pen_test.go
@@ -1,18 +1,18 @@
 package pen
 
 import (
-  "time"
-  "testing"
+	"testing"
+	"time"
 )
 
 func TestPen(t *testing.T) {
-  pen := New("TEST", "/tmp/pen-test-log.%Y%m%d")
+	pen := New("TEST", "/tmp/pen-test-log.%Y%m%d-%S")
 
-  pen.Write("pen-test-1")
-  time.Sleep(time.Second * 2)
+	pen.Write("pen-test-1")
+	time.Sleep(time.Second * 2)
 
-  pen.Write("pen-test-2")
-  time.Sleep(time.Second * 2)
+	pen.Write("pen-test-2")
+	time.Sleep(time.Second * 2)
 
-  pen.Lid()
+	pen.Lid()
 }
